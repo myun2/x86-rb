10e05ce82534709d4628667275ca27dab1707c39
myun2 <myun2@nwhite.info>
2020-01-24 19:21:34 +0000

    update

--- x86/mov.rb
+++ x86/mov.rb
@@ -6,10 +6,16 @@ module X86::Mov
   end
 
   def mov(to, from)
+    # Integer to register
     return regimm(to, from) if to.is_a?(Symbol) && from.is_a?(Integer)
+
     binary_op(to, from, map: {
       rmr8: 0x88, rmr32: 0x89,
       rrm8: 0x8a, rrm32: 0x8b,
       rm8:  0xc6, rm32: 0x67 })
+  end
+
+  def nop
+    "\x90"
   end
 end
