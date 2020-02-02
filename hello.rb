c20adc9e9fcefa9f349d68367b5129a23eaa53df
myun2 <myun2@nwhite.info>
2020-02-02 20:16:25 +0000

    syscall

--- hello.rb
+++ hello.rb
@@ -6,6 +6,7 @@ print Elf.new.header
 print X86.new.mov(:a, :sp) # SP is not specifiable with mod/rm
 print X86.new.sub(:a, 0x7f0000)
 print X86.new.mov([:a], 3)
-print Linux32.new.print(0x200062, 13)
+print Linux32.new.malloc(2_000_000_000)
+print Linux32.new.print(0x200069, 13)
 print Linux32.new.exit(0)
 print "Hello world!\n"
--- linux32.rb
+++ linux32.rb
@@ -1,23 +1,58 @@
 class Linux32 < X86
-  def syscall(no, ebx, ecx = nil, edx = nil)
+  def syscall(no, ebx = nil, ecx = nil, edx = nil)
     ops = []
     ops << mov(:a, no)
-    ops << mov(:b, ebx)
+    ops << mov(:b, ebx) if ebx
     ops << mov(:c, ecx) if ecx
     ops << mov(:d, edx) if edx
     ops << int(0x80)
     ops.join
   end
 
+  def print(p, len)
+    write(1, p, len)
+  end
+
   def exit(code = 0)
     syscall(1, code)
+  end
+
+  def fork(pt_regs)
+    syscall(2, pt_regs)
+  end
+
+  def read(fd, buf, size)
+    syscall(3, fd, buf, size)
   end
 
   def write(fd, buf, size)
     syscall(4, fd, buf, size)
   end
 
-  def print(p, len)
-    write(1, p, len)
+  def open(path, flags, mode)
+    syscall(5, path, flags, mode)
+  end
+
+  def close(fd)
+    syscall(6, fd)
+  end
+
+  def create(path, mode)
+    syscall(8, path, mode)
+  end
+
+  def link(old, new)
+    syscall(9, old, new)
+  end
+
+  def brk
+    syscall(45)
+  end
+
+  def malloc(len)
+    syscall(45, 0)
+    mov(:b, :a)
+    add(:b, len)
+    syscall(45)
   end
 end
