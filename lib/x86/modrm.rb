de6ee8a804e73ad3c310cf749b8690d8c17be422
myun2 <myun2@nwhite.info>
2020-02-20 05:09:42 +0000

    sib

--- lib/x86/modrm.rb
+++ lib/x86/modrm.rb
@@ -14,21 +14,6 @@ module X86::ModRM
     end
   end
 
-  def regno(reg)
-    return reg if reg.is_a? Integer
-    case reg
-    when :a then 0
-    when :c then 1
-    when :d then 2
-    when :b then 3
-    when :sp then 4
-    when :bp then 5
-    when :si then 6
-    when :di then 7
-    else raise "Unknown reg name #{reg}"
-    end
-  end
-
   def mod(mode)
     case mode
     when :ptr then 0
