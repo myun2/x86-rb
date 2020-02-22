d2294b4ef45bb82312091e2bc73827790174674b
myun2 <myun2@nwhite.info>
2020-02-22 09:57:08 +0000

    Emulator GPR

--- lib/x86/emulator/registers.rb
+++ lib/x86/emulator/registers.rb
@@ -1,13 +1,8 @@
+require 'x86/emulator/gpr'
+
 class X86::Emulator::Registers
   def initialize
-    @a = 0
-    @c = 0
-    @d = 0
-    @b = 0
-    @sp = 0
-    @bi = 0
-    @si = 0
-    @di = 0
+    @gpr = X86::Emulator::Gpr
     @eflags = 0
   end
 end
